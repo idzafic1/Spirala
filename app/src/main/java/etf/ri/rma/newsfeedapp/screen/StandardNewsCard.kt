c77b01d4d8232f3598c891da91b3cbea26804da4
spiki10 <ismaildzafic85@gmail.com>
2025-04-06 16:03:00 +0200

    Prvi Commit

---
+++ app/src/main/java/etf/ri/rma/newsfeedapp/screen/StandardNewsCard.kt
@@ -0,0 +1,62 @@
+package etf.ri.rma.newsfeedapp.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import etf.ri.rma.newsfeedapp.R
+import etf.ri.rma.newsfeedapp.model.NewsItem
+
+@Composable
+fun StandardNewsCard(newsItem: NewsItem) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "IMG",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(8.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = newsItem.title,
+                    style = MaterialTheme.typography.titleLarge,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                Text(
+                    text = newsItem.snippet,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "${newsItem.source} • ${newsItem.publishedDate}",
+                    style = MaterialTheme.typography.labelSmall,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+    }
+}
