871e2eca5517a8ec25292cf7596ec622086772f4
spiki10 <ismaildzafic85@gmail.com>
2025-04-12 23:35:11 +0200

    Obrisani fajlovi

--- SpiralaCopy/app/src/main/java/etf/ri/rma/newsfeedapp/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package etf.ri.rma.newsfeedapp.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val LightBackground = Color(0xFFFFFFFF)
-val DarkBackground = Color(0xFF121212)
-val ChipBackgroundDark = Color(0xFF1F1F1F)
-val ChipTextDark = Color(0xFFFFFFFF)
